0b02474b6f8b4ba75a3a62c962fc762598f7f60f
Patrycja Ińska <patrycja.inska@interia.pl>
2022-07-03 22:36:56 +0200

    Configuration and connection to the MySQL database.

---
+++ config/config.ts
@@ -0,0 +1,7 @@
+export const config = {
+    dbHost: 'localhost',
+    dbUser: 'root',
+    dbPassword: '',
+    dbDatabase: 'megak_budget_app',
+    corsOrigin: 'http://localhost:3000',
+};
--- index.ts
+++ index.ts
@@ -2,11 +2,12 @@ import express, {json} from "express";
 import cors from 'cors';
 import 'express-async-errors';
 import rateLimit from 'express-rate-limit'
+import {config} from "./config/config";
 
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: config.corsOrigin,
 }));
 
 app.use(json());
