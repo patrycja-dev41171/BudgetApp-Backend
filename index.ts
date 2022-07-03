d4e7bf3f8ab2017bbf9454cc9660a9d85594f1e2
Patrycja Ińska <patrycja.inska@interia.pl>
2022-07-03 22:28:40 +0200

    A local server has been created and some settings have been made, for example: cors, rate-limit.

---
+++ index.ts
@@ -0,0 +1,26 @@
+import express, {json} from "express";
+import cors from 'cors';
+import 'express-async-errors';
+import rateLimit from 'express-rate-limit'
+
+const app = express();
+
+app.use(cors({
+    origin: 'http://localhost:3000',
+}));
+
+app.use(json());
+
+app.use(rateLimit({
+    windowMs: 5 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+}));
+
+app.get('/', (req, res) => {
+    res.send('<h1>It works!</h1>')
+})
+
+
+app.listen(3001, '0.0.0.0', () => {
+    console.log('Listening on port http://localhost:3001');
+});
