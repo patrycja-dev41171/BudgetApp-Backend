e84a97c3e5e17c73c9cdb579d84c138a7ce91b4a
Patrycja Ińska <patrycja.inska@interia.pl>
2022-07-04 17:01:49 +0200

    registerRouter. First router was created and endpoint with POTS method added. Checking if the user already has an account in the application, data hashing (bcrypt) and adding the user to the database.

--- index.ts
+++ index.ts
@@ -1,30 +1,32 @@
-import express, {json} from "express";
-import cors from 'cors';
-import 'express-async-errors';
-import rateLimit from 'express-rate-limit'
-import {config} from "./config/config";
-import {handleError} from "./utils/error";
+import express, { json } from "express";
+import cors from "cors";
+import "express-async-errors";
+import rateLimit from "express-rate-limit";
+import { config } from "./config/config";
+import { handleError } from "./utils/error";
+import { registerRouter } from "./routers/register.router";
 
 const app = express();
 
-app.use(cors({
+app.use(
+  cors({
     origin: config.corsOrigin,
-}));
+  })
+);
 
 app.use(json());
 
-app.use(rateLimit({
+app.use(
+  rateLimit({
     windowMs: 5 * 60 * 1000, // 15 minutes
     max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-}));
+  })
+);
+
+app.use("/register", registerRouter);
 
 app.use(handleError);
 
-app.get('/', (req, res) => {
-    res.send('<h1>It works!</h1>')
-})
-
-
-app.listen(3001, '0.0.0.0', () => {
-    console.log('Listening on port http://localhost:3001');
+app.listen(3001, "0.0.0.0", () => {
+  console.log("Listening on port http://localhost:3001");
 });
---
+++ routers/register.router.ts
@@ -0,0 +1,29 @@
+import { Router } from "express";
+import { UserRecord } from "../records/user.record";
+import { hash } from "bcrypt";
+import { ValidationError } from "../utils/error";
+
+export const registerRouter = Router().post("/", async (req, res) => {
+  const { email, password } = req.body;
+  const result = await UserRecord.getOne(email);
+
+  if (result !== null) {
+    throw new ValidationError(
+      "An account with such an e-mail has already been created. Enter a new e-mail address or log in."
+    );
+
+  } else {
+    hash(password, 10, async (err: Error, password: string) => {
+      if (err) {
+        console.log(err);
+      } else {
+        const user = new UserRecord({
+          ...req.body,
+          password
+        });
+        await user.insert();
+        res.json(user.id);
+      }
+    });
+  }
+});
